ba7999f356601c849bb645e6b0eb454551d37761
Abhishek01039 <abhishekghaskata1999@gmail.com>
2021-02-07 09:00:45 +0530

    Added integration test

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -1,36 +1,49 @@
-// // This is a basic Flutter integration test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter integration test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:integration_test/integration_test.dart';
+import 'dart:io';
 
-// import 'package:example/main.dart' as app;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
-// void main() => run(_testMain);
+import 'package:example/main.dart' as app;
 
-// void _testMain() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     app.main();
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-//     // Trigger a frame.
-//     await tester.pumpAndSettle();
+  testWidgets('testing `OnBoardingScreen` widget', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+    sleep(Duration(seconds: 3));
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+    await tester.drag(
+        find.text('Developer Student Club'), const Offset(-300, 0.0));
+    await tester.pumpAndSettle();
 
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+    sleep(Duration(seconds: 3));
+
+    await tester.drag(
+        find.text('Developer Student Club'), const Offset(-300, 0.0));
+    await tester.pumpAndSettle();
+
+    sleep(Duration(seconds: 3));
+
+    await tester.drag(
+        find.text('Developer Student Club'), const Offset(-300, 0.0));
+    await tester.pumpAndSettle();
+
+    sleep(Duration(seconds: 3));
+
+    await tester.drag(
+        find.text('Developer Student Club'), const Offset(-300, 0.0));
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(Key('get_started')), findsOneWidget);
+  });
+}
