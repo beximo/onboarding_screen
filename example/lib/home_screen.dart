9b748163a558fce5161eedd2169cda1e007bc674
Ankit Sagar <62764278+woinbo@users.noreply.github.com>
2021-03-11 21:42:29 +0530

    update

--- example/lib/home_screen.dart
+++ example/lib/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: need to refectore this
     return Scaffold(
       body: const Center(
         child: Text('Home Page'),
